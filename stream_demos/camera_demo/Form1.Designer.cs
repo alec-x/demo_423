12a765c604fd4af82a74857699339c631c7f5b66
Alec Xu <Alec_Xu@hotmail.com>
2019-07-02 13:05:18 -0700

    restructure file structure

---
+++ stream_demos/camera_demo/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace camera_demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.PreviewBox = new System.Windows.Forms.PictureBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PreviewBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PreviewBox
+            // 
+            this.PreviewBox.Location = new System.Drawing.Point(143, 97);
+            this.PreviewBox.Name = "PreviewBox";
+            this.PreviewBox.Size = new System.Drawing.Size(432, 248);
+            this.PreviewBox.TabIndex = 0;
+            this.PreviewBox.TabStop = false;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(73, 380);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 1;
+            this.StartButton.Text = "start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(666, 371);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 23);
+            this.StopButton.TabIndex = 2;
+            this.StopButton.Text = "stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.PreviewBox);
+            this.Name = "Form1";
+            this.Text = "Video Stream Demo";
+            ((System.ComponentModel.ISupportInitialize)(this.PreviewBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.PictureBox PreviewBox;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+    }
+}
+
---
+++ stream_demos/camera_demo/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Accord.Video.DirectShow;
+using Accord.Video;
+using System.IO;
+
+namespace camera_demo
+{
+    public partial class Form1 : Form
+    {
+
+        // collection of available video devices
+        private FilterInfoCollection videoDevices;
+        // selected video device
+        private VideoCaptureDevice videoDevice;
+
+        // TODO: Remove this test var
+        private Int64 count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeVideoDevice();
+        }
+
+        public void InitializeVideoDevice()
+        {
+            // Find first valid video input device (ie. webcam) and assign as videodevice
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            videoDevice = new VideoCaptureDevice(videoDevices[1].MonikerString);
+            videoDevice.NewFrame += new NewFrameEventHandler(FrameHandler);
+        }
+
+        private void FrameHandler(object sender, NewFrameEventArgs args)
+        {
+            Bitmap newFrame = args.Frame;
+            string prefix = Environment.CurrentDirectory;
+            string name = prefix + @"\output\" + "lol" + count + ".bmp";
+            newFrame.Save(name, System.Drawing.Imaging.ImageFormat.Bmp);
+            count++;
+                
+            //PreviewBox.Image = newFrame;
+            return;
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            videoDevice.Start();
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            videoDevice.Stop();
+        }
+    }
+}
