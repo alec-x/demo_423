025061e150d6f60ba3149387341fa446c54cf23e
Alec Xu <Alec_Xu@hotmail.com>
2019-07-02 11:05:44 -0700

    added accord.NET Direct Show implementation

--- stream_demo/stream_demos/camera_demo/Form1.cs
+++ stream_demo/stream_demos/camera_demo/Form1.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Accord.Video.DirectShow;
+using Accord.Video;
 namespace camera_demo
 {
     public partial class Form1 : Form
